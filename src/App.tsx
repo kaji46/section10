73d86efa567992c3ee9a45babbca94d6cfd3943f
Naoto Kajimura <naotokajimura@gmail.com>
2021-07-24 14:19:39 +0900

    type script finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
 
 export default function App() {
+  const user: User = {
+    name: "aaa"
+    // hobbies: ["映画", "ゲーム"]
+  };
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          completed={todo.completed}
+          title={todo.title}
+          userId={todo.userId}
+        />
+      ))}
     </div>
   );
 }
